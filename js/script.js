4caaf9c3e003f61905fbeffa5b16fa16ce93d822
Damian Bregier <damian.bregier@gmail.com>
2020-06-15 18:11:55 +0200

    Configuration of pie-charts and carousel

--- js/script.js
+++ js/script.js
@@ -19,22 +19,33 @@ $(document).ready(function(){
     //Activate skills carousel 
     $('.owl-carousel').owlCarousel({
         loop:true,
-        margin:10,
-        nav:true,
+        items: 4,
         responsive:{
             0:{
                 items:1
             },
-            600:{
+            480:{
+                items:2
+            },
+            768:{
                 items:3
             },
-            1000:{
-                items:5
+            966:{
+                items:4
             }
         }
     })
     $('.chart').easyPieChart({
-        //your configuration goes here
+        easing: 'easeInOut',
+        barColor: '#fff',
+        trackColor: false,
+        scaleColor: false,
+        lineWidth: 4,
+        size: 152,
+        onStep: function(from, to, percent){
+            $(this.el).find('.percent').text(Math.round(percent));
+        }
+
     });
 
 });
