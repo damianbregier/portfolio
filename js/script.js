d441d569c03322f7e17734f035bf84bc63ff6cf1
Damian Bregier <damian.bregier@gmail.com>
2020-06-15 13:26:23 +0200

    Add properties to slider

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 //Read this fuction when document (page) is ready
 $(document).ready(function(){
-
-    $('#slides').superslides();
+    //Activates superslide
+    $('#slides').superslides({
+        animation: 'fade',
+        play: 7000,
+        pagination: false
+    });
 
 });
