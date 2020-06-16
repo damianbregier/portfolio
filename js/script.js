34b7799776c4c3aedf81d25fa4681a3983976d29
Damian Bregier <damian.bregier@gmail.com>
2020-06-16 14:31:30 +0200

    Add smooth jump to section

--- js/script.js
+++ js/script.js
@@ -99,6 +99,17 @@ $(document).ready(function () {
         });
 
         return false;
+    });
+
+    $("#navigation li a").click(function(e){
+        e.preventDefault();
+
+        var targetElement = $(this).attr("href");
+        var targetPosition = $(targetElement).offset().top;
+        $("html, body").animate({
+            scrollTop: targetPosition -50
+        },"slow");
+
 
     });
 
