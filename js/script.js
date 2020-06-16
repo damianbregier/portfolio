fedbdbabea0ab5f865627689469a93eb06ee3174
Damian Bregier <damian.bregier@gmail.com>
2020-06-16 11:39:33 +0200

    Add and style icons to pictures

--- js/script.js
+++ js/script.js
@@ -74,6 +74,14 @@ $(document).ready(function () {
     });
     //Activates fancy box
     $("[data-fancybox]").fancybox();
+    $(".items").isotope({
+        filter: '*',
+        animationOptions: {
+            duration: 1500,
+            easing: 'linear',
+            queue: false
+        }
+    })
 
 
 
